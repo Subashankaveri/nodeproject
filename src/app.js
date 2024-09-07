a924fc7b3c57575d000b01d637adde99053dfe0e
Subashankaveri <159506907+Subashankaveri@users.noreply.github.com>
2024-09-08 00:46:49 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -20,6 +20,6 @@ app.use('/', blogRoutes);
 
 // Start the server
 const PORT = process.env.APP_PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
